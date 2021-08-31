89076a672c32e5a596582742bda06288b5825a3a
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2021-09-01 02:39:04 +0530

    main header added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-window.alert("Welcome Page.");
+// header section 
+$(document).ready(function() {    
+     $(".gnb__dimmed").addClass('open');
+    $(".gnb__dimmed").css({'display':'block'});
+
+      $("#login-btn").hover( function () {
+          $(".gnb__login-layer").addClass("active");
+      }, function() {
+        $(".gnb__login-layer").removeClass("active");
+      });
+});
